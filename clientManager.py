b5e177359a587c102c8bf807e33e36456213e530
beva3 <mizabienvenu@gmail.com>
2024-11-09 15:26:56 +0530

    add_client()

--- clientManager.py
+++ clientManager.py
@@ -4,3 +4,7 @@ class ClientManager:
     def __init__(self):
         print("Init ClientManager ...")
         self.clients = []
+
+    def add_client(self, client):
+        self.clients.append(client)
+        print(f"Client {client.name} added successfully.")
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from client import Client
 from clientManager import ClientManager
 
+# creat an instance of ClientManager
 manager = ClientManager()
+
+# client 
+clients = [
+    Client(1, "John Doe", "john.doe@example.com", "1234567890"),
+    Client(2, "Jane Smith", "jane.smith@example.com", "0987654321"),
+    Client(3, "Alice Johnson", "alice.johnson@example.com", "9876543210")  # duplicate id, this will cause an error
+]
+
+# add somme client
+for client in clients:
+    manager.add_client(client)
+
